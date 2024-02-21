cc913b867270e4837b255f19216b5cb38a4ed03b
gaji <zarinelle.c@gmail.com>
2024-02-21 23:12:19 +0800

    Intial Commit

---
+++ app/src/main/java/com/example/test3/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.test3;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView add_noteBTN = findViewById(R.id.add_noteBTN);
+
+        add_noteBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(MainActivity.this);
+                bottomSheetDialog.setContentView(R.layout.activity_create_task);
+                bottomSheetDialog.show();
+            }
+        });
+
+    }
+}
